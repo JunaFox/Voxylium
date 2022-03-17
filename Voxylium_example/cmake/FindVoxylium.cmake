7e1f4a5264e2e387ae7273aaef9a8d81d17cbf71
udypz <udypz@student.kit.edu>
2022-03-17 21:06:28 +0100

    add cmake configuration

---
+++ Voxylium_example/cmake/FindVoxylium.cmake
@@ -0,0 +1,9 @@
+# paths may need to be changed depending on the setup
+set(FIND_VOXYLIUM_PATHS ../Voxylium)
+
+find_path(VOXYLIUM_INCLUDE_DIR voxylium.h
+        PATHS ${FIND_VOXYLIUM_PATHS})
+
+find_library(VOXYLIUM_LIBRARY
+             NAMES libvoxylium
+             PATHS ${FIND_VOXYLIUM_PATHS}/cmake-build-release)
